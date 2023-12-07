f28ba5b242ac014e9ac1dc1c5a55502a6de583ed
atulkumarbluedd <atul.arya1993@gmail.com>
2023-12-07 11:09:49 +0530

    s

--- TestProject/src/test/java/SeleniumHandsOn/Selenium_grid_usingDocker.java
+++ TestProject/src/test/java/SeleniumHandsOn/Selenium_grid_usingDocker.java
@@ -2,57 +2,45 @@ package SeleniumHandsOn;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.CompletableFuture;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import bsh.Capabilities;
 import io.appium.java_client.remote.MobileBrowserType;
+import jdk.jfr.DataAmount;
 
-public class sampleTest {
-	@Test
-	public void sma() throws MalformedURLException, InterruptedException {
-		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.setCapability("browserVersion", "100");
-		chromeOptions.setCapability("platformName", "linux");
+public class Selenium_grid_usingDocker {
 
-		// Showing a test name instead of the session id in the Grid UI
-		chromeOptions.setCapability("se:name", "My simple test");
-		// Other type of metadata can be seen in the Grid UI by clicking on the
-		// session info or via GraphQL
-		chromeOptions.setCapability("se:sampleMetadata", "Sample metadata value");
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4446/wd/hub"), chromeOptions);
-		driver.get("http://www.amazon.com");
-		Thread.sleep(80000);
-		driver.quit();
-	}
-
-	@Test(description = "below test is to run tests in selenium grid")
-	public void executeTestsIn_selenium_grid() throws Exception {
+	/**
+	 * here prerequisite us to install every corresponding version image from docker
+	 * 
+	 * @param browserName
+	 * @param version
+	 * @throws Exception
+	 */
+	@Test(description = "Execute tests in parallel with different versions in different browers using selenium hub and node", dataProvider = "getData")
+	public void executeTestsIn_selenium_grid_using_dataProvider(String browserName, String version) throws Exception {
 		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setBrowserName("chrome");
+		cap.setBrowserName(browserName);
+//		cap.setVersion(version);
 
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4446/wd/hub"), cap);
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
 		driver.get("https://google.com");
 		System.out.println("title is >> " + driver.getTitle());
-
+		 
 		Thread.sleep(70000);
 		driver.quit();
 	}
 
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setBrowserName("chrome");
-
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4446/wd/hub"), cap);
-		driver.get("https://google.com");
-		System.out.println("title is >> " + driver.getTitle());
-		Thread.sleep(100000);
-		driver.get("https://google.com");
-		driver.quit();
+	@DataProvider(parallel = true)
+	public Object[][] getData() {
+		return new Object[][] { { "chrome", "119" }, { "firefox", "400" }, { "chrome", "119" }, { "firefox", "119" } };
 	}
 }
