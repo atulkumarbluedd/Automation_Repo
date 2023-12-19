e4ee2b2b093d1b5e0c0ef1dc3f0e427c49f4f440
atulkumarbluedd <atul.arya1993@gmail.com>
2023-12-19 11:57:17 +0530

    s

--- TestProject/src/test/java/SeleniumHandsOn/Factories/ReadPropertyFile.java
+++ TestProject/src/test/java/SeleniumHandsOn/Factories/ReadPropertyFile.java
@@ -9,33 +9,33 @@ import java.util.Properties;
 import SeleniumHandsOn.ConfigSource.constants;
 
 public class ReadPropertyFile {
-	private ReadPropertyFile() {
+    private ReadPropertyFile() {
+        // this is to restrict any class to create object
+    }
 
-	}
+    /**
+     * Here we are using static block so that property file is loaded once into the
+     * map and then test case can simply leverage this map to get the value. as
+     * loading the property file takes time.
+     */
 
-	/**
-	 * Here we are using static block so that property file is loaded once into the
-	 * map and then test case can simply leverage this map to get the value. as
-	 * loading the property file takes time.
-	 */
+    private static Properties properties = new Properties();
+    private static final Map<String, String> CONFIGMAP = new HashMap<>();
 
-	private static Properties properties = new Properties();
-	private static final Map<String, String> CONFIGMAP = new HashMap<>();
-	static {
-		try {
-			FileInputStream fis = new FileInputStream(constants.getCONFIG_FILE_PATH());
-			properties.load(fis);
-			properties.entrySet()
-					.forEach(entry -> CONFIGMAP.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue())));
+    static {
+        try {
+            FileInputStream fis = new FileInputStream(constants.getCONFIG_FILE_PATH());
+            properties.load(fis);
+            properties.forEach((key, value) -> CONFIGMAP.put(String.valueOf(key), String.valueOf(value).trim()));
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static String get(String key) throws Exception {
-		if (Objects.isNull(key) || Objects.isNull(CONFIGMAP.get(key)))
-			throw new Exception("property name " + key + "is not found. Please check config.properties !! ");
-		return CONFIGMAP.get(key);
-	}
+    public static String get(String key) throws Exception {
+        if (Objects.isNull(key) || Objects.isNull(CONFIGMAP.get(key)))
+            throw new Exception(STR."property name \{key} is not found. Please check config.properties !! ");
+        return CONFIGMAP.get(key);
+    }
 }
