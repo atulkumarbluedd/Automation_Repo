6d7fc240389775c2e719f532aafaf401cd90cb62
atulkumarbluedd <atul.arya1993@gmail.com>
2023-12-20 17:48:33 +0530

    s

--- TestProject/src/test/java/SeleniumHandsOn/Factories/textFileReader.java
+++ TestProject/src/test/java/SeleniumHandsOn/Factories/textFileReader.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class textFileReader {
@@ -17,8 +18,27 @@ public class textFileReader {
         reader.close();
     }
 
+    public static void textFileWriter(String path) {
+        try (FileWriter writer = new FileWriter(path)) {
+            writer.write("new text added");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void appendTextFile(String path) {
+        try (FileWriter writer = new FileWriter(path,true)) {
+            writer.write("appended text");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
     @Test(description = "this test is to show how this utility works !!")
     public void fileReader() throws IOException {
         textFileReaderUtil("testing.txt");
+        appendTextFile("sample.txt");
+        textFileWriter("sample.txt");
+        textFileReaderUtil("sample.txt");
     }
 }
